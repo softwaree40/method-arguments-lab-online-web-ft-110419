a198f32786dfe05124607f067b6dd9f6ff4f7168
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-04 17:10:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
    puts "Hi, my name is #{name}."
 end
 def introduction_with_language(name , language)
+<<<<<<< HEAD
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+  puts ""
+>>>>>>> bb6a01b8c401c0e1d758940220439505bcdac7f1
   
 end
